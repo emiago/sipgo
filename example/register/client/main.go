e899d0aeedbb61a95201932f9c54253e25270576
Emir Aganovic <emirfreelance91@gmail.com>
2023-02-02 23:42:21 +0100

    Adding auth example #6

---
+++ example/register/client/main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/emiago/sipgo"
+	"github.com/emiago/sipgo/parser"
+	"github.com/emiago/sipgo/sip"
+	"github.com/emiago/sipgo/transport"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/icholy/digest"
+)
+
+func main() {
+	extIP := flag.String("ip", "127.0.0.50:5060", "My exernal ip")
+	dst := flag.String("srv", "127.0.0.10:5060", "Destination")
+	username := flag.String("u", "alice", "SIP Username")
+	password := flag.String("p", "alice", "Password")
+	sipdebug := flag.Bool("sipdebug", false, "Turn on sipdebug")
+	flag.Parse()
+
+	// Make SIP Debugging available
+	transport.SIPDebug = *sipdebug
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMicro
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.StampMicro,
+	}).With().Timestamp().Logger().Level(zerolog.DebugLevel)
+
+	// Setup UAC
+	ua, err := sipgo.NewUA(
+		sipgo.WithUserAgent(*username),
+		sipgo.WithUserAgentIP(*extIP),
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup user agent")
+	}
+
+	srv, err := sipgo.NewServer(ua)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup server handle")
+	}
+
+	client, err := sipgo.NewClient(ua)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup client handle")
+	}
+
+	ctx := context.TODO()
+	go srv.ListenAndServe(ctx, "udp", *extIP)
+
+	// Wait that ouir server loads
+	time.Sleep(1 * time.Second)
+
+	// Create basic REGISTER request structure
+	recipient := &sip.Uri{}
+	parser.ParseUri(fmt.Sprintf("sip:%s@%s", *username, *dst), recipient)
+	req := sip.NewRequest(sip.REGISTER, recipient, "SIP/2.0")
+	req.AppendHeader(&sip.GenericHeader{HeaderName: "Contact", Contents: fmt.Sprintf("<sip:%s@%s>", *username, *extIP)})
+
+	// Send request and parse response
+	// req.SetDestination(*dst)
+	tx, err := client.TransactionRequest(req.Clone())
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to create transaction")
+	}
+	defer tx.Terminate()
+
+	res, err := getResponse(tx)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to get response")
+	}
+
+	log.Info().Int("status", int(res.StatusCode())).Msg("Received status")
+	if res.StatusCode() == 401 {
+		// Get WwW-Authenticate
+		wwwAuth := res.GetHeader("WWW-Authenticate")
+		chal, err := digest.ParseChallenge(wwwAuth.Value())
+		if err != nil {
+			log.Fatal().Str("wwwauth", wwwAuth.Value()).Err(err).Msg("Fail to parse challenge")
+		}
+
+		// Reply with digest
+		cred, _ := digest.Digest(chal, digest.Options{
+			Method:   req.Method.String(),
+			URI:      recipient.Host,
+			Username: *username,
+			Password: *password,
+		})
+
+		newReq := req.Clone()
+		newReq.AppendHeader(&sip.GenericHeader{
+			HeaderName: "Authorization",
+			Contents:   cred.String(),
+		})
+
+		tx, err = client.TransactionRequest(newReq)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Fail to create transaction")
+		}
+		defer tx.Terminate()
+
+		res, err = getResponse(tx)
+		if err != nil {
+			log.Fatal().Err(err).Msg("Fail to get response")
+		}
+	}
+
+	if res.StatusCode() != 200 {
+		log.Fatal().Msg("Fail to register")
+	}
+
+	log.Info().Msg("Client registered")
+}
+
+func getResponse(tx sip.ClientTransaction) (*sip.Response, error) {
+	select {
+	case <-tx.Done():
+		return nil, fmt.Errorf("transaction died")
+	case res := <-tx.Responses():
+		return res, nil
+	}
+}
---
+++ example/register/server/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/emiago/sipgo"
+	"github.com/emiago/sipgo/sip"
+	"github.com/emiago/sipgo/transport"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+
+	"github.com/icholy/digest"
+)
+
+func main() {
+	extIP := flag.String("ip", "127.0.0.1:5060", "My exernal ip")
+	creds := flag.String("u", "alice:alice", "Coma seperated username:password list")
+	sipdebug := flag.Bool("sipdebug", false, "Turn on sipdebug")
+	flag.Parse()
+
+	// Make SIP Debugging available
+	transport.SIPDebug = *sipdebug
+
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMicro
+	log.Logger = zerolog.New(zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.StampMicro,
+	}).With().Timestamp().Logger().Level(zerolog.DebugLevel)
+
+	registry := make(map[string]string)
+	for _, c := range strings.Split(*creds, ",") {
+		arr := strings.Split(c, ":")
+		registry[arr[0]] = arr[1]
+	}
+
+	ua, err := sipgo.NewUA(
+		sipgo.WithUserAgent("SIPGO"),
+	// sipgo.WithUserAgentIP(*extIP),
+	)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup user agent")
+	}
+
+	srv, err := sipgo.NewServer(ua)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Fail to setup server handle")
+	}
+
+	ctx := context.TODO()
+
+	// NOTE: This server only supports 1 REGISTRATION/Chalenge
+	// This needs to be rewritten in better way
+	var chal digest.Challenge
+	srv.OnRegister(func(req *sip.Request, tx sip.ServerTransaction) {
+		// https://www.rfc-editor.org/rfc/rfc2617#page-6
+		h := req.GetHeader("Authorization")
+		if h == nil {
+			chal = digest.Challenge{
+				Realm:     "sipgo-server",
+				Nonce:     fmt.Sprintf("%d", time.Now().UnixMicro()),
+				Opaque:    "sipgo",
+				Algorithm: "MD5",
+			}
+
+			res := sip.NewResponseFromRequest(req, 401, "Unathorized", nil)
+			res.AppendHeader(&sip.GenericHeader{
+				HeaderName: "WWW-Authenticate",
+				Contents:   chal.String(),
+			})
+
+			tx.Respond(res)
+			return
+		}
+
+		cred, err := digest.ParseCredentials(h.Value())
+		if err != nil {
+			log.Error().Err(err).Msg("parsing creds failed")
+			tx.Respond(sip.NewResponseFromRequest(req, 401, "Bad credentials", nil))
+			return
+		}
+
+		// Check registry
+		passwd, exists := registry[cred.Username]
+		if !exists {
+			tx.Respond(sip.NewResponseFromRequest(req, 404, "Bad authorization header", nil))
+			return
+		}
+
+		// Make digest and compare response
+		digCred, err := digest.Digest(&chal, digest.Options{
+			Method:   "REGISTER",
+			URI:      cred.URI,
+			Username: cred.Username,
+			Password: passwd,
+		})
+
+		if err != nil {
+			log.Error().Err(err).Msg("Calc digest failed")
+			tx.Respond(sip.NewResponseFromRequest(req, 401, "Bad credentials", nil))
+			return
+		}
+
+		if cred.Response != digCred.Response {
+			tx.Respond(sip.NewResponseFromRequest(req, 401, "Unathorized", nil))
+			return
+		}
+		log.Info().Str("username", cred.Username).Str("source", req.Source()).Msg("New client registered")
+		tx.Respond(sip.NewResponseFromRequest(req, 200, "OK", nil))
+	})
+
+	srv.ListenAndServe(ctx, "udp", *extIP)
+}
