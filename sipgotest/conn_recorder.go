75f3618874adfd031b5a6ed81da7e0f8d25c7789
Emir Aganovic <emirfreelance91@gmail.com>
2023-10-16 03:28:14 +0200

    feat: adding sipgotest for testing handlers

---
+++ sipgotest/conn_recorder.go
@@ -0,0 +1,35 @@
+package sipgotest
+
+import (
+	"net"
+	"sync/atomic"
+
+	"github.com/emiago/sipgo/sip"
+)
+
+type connRecorder struct {
+	msgs []sip.Message
+
+	ref atomic.Int32
+}
+
+func newConnRecorder() *connRecorder {
+	return &connRecorder{}
+}
+
+func (c *connRecorder) LocalAddr() net.Addr {
+	return nil
+}
+
+func (c *connRecorder) WriteMsg(msg sip.Message) error {
+	c.msgs = append(c.msgs, msg)
+	return nil
+}
+func (c *connRecorder) Ref(i int) int {
+	return int(c.ref.Add(int32(i)))
+}
+func (c *connRecorder) TryClose() (int, error) {
+	new := c.ref.Add(int32(-1))
+	return int(new), nil
+}
+func (c *connRecorder) Close() error { return nil }
---
+++ sipgotest/server_tx_recorder.go
@@ -0,0 +1,66 @@
+package sipgotest
+
+import (
+	"github.com/emiago/sipgo/sip"
+	"github.com/emiago/sipgo/transaction"
+
+	"github.com/rs/zerolog/log"
+)
+
+func NewServerTxRecorder(req *sip.Request) *ServerTxRecorder {
+	// stx := transaction.NewServerTx()
+
+	key, err := transaction.MakeServerTxKey(req)
+	if err != nil {
+		panic(err)
+	}
+	conn := newConnRecorder()
+	stx := transaction.NewServerTx(key, req, conn, log.Logger)
+	if err := stx.Init(); err != nil {
+		panic(err)
+	}
+	return &ServerTxRecorder{
+		stx,
+		conn,
+	}
+}
+
+// ServerTxRecorder wraps server transactions
+type ServerTxRecorder struct {
+	*transaction.ServerTx
+	c *connRecorder
+}
+
+// Result returns sip response. Can be nil if none was processed
+func (r *ServerTxRecorder) Result() *sip.Response {
+	if len(r.c.msgs) == 0 {
+		return nil
+	}
+	return r.c.msgs[0].(*sip.Response).Clone()
+}
+
+// func (r *ServerTxRecorder) Terminate() {
+
+// }
+
+// func (r *ServerTxRecorder) Done() <-chan struct{} {
+
+// }
+
+// func (r *ServerTxRecorder) Err() error {
+
+// }
+
+// func (r *ServerTxRecorder) Respond(res *sip.Response) error {
+
+// }
+
+// func (r *ServerTxRecorder) Acks() <-chan *sip.Request {
+
+// }
+
+// func (r *ServerTxRecorder) Cancels() <-chan *sip.Request {
+
+// }
+
+// var _ sip.ServerTransaction = &ServerTxRecorder{}
