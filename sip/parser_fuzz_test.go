a3fbce873f079782ddbaeb6beb9aa88b0dde44ff
Emir Aganovic <emirfreelance91@gmail.com>
2025-02-10 23:48:54 +0100

    fix: remove final bits of zerolog

--- sip/parser_fuzz_test.go
+++ sip/parser_fuzz_test.go
@@ -7,15 +7,6 @@ import (
 
 func FuzzParseSipMessage(f *testing.F) {
 	// Creates Race when run
-	// log.Logger = zerolog.New(zerolog.ConsoleWriter{
-	// 	Out:        os.Stdout,
-	// 	TimeFormat: "2006-01-02 15:04:05.000",
-	// }).With().Timestamp().Logger().Level(zerolog.WarnLevel)
-
-	// if lvl, err := zerolog.ParseLevel(os.Getenv("LOG_LEVEL")); err == nil {
-	// 	log.Logger = log.Level(lvl)
-	// }
-
 	rawMsg := []string{
 		"INVITE sip:bob@127.0.0.1:5060 SIP/2.0",
 		"Via: SIP/2.0/UDP 127.0.0.2:5060;branch=z9hG4bK.VYWrxJJyeEJfngAjKXELr8aPYuX8tR22",
