be7c0f45f373663d60093d8fb31a205cb0b1a9b4
Emir Aganovic <emirfreelance91@gmail.com>
2025-09-30 18:55:12 +0200

    fix: allow BYE for early dialogs

--- dialog_client.go
+++ dialog_client.go
@@ -434,9 +434,10 @@ func (s *DialogClientSession) WriteBye(ctx context.Context, bye *sip.Request) er
 		return nil
 	}
 
-	// In case dialog was not updated
-	if sip.DialogState(state) != sip.DialogStateConfirmed {
-		return fmt.Errorf("Dialog not confirmed. ACK not send?")
+	// 	The caller's UA MAY send a BYE for either
+	//    confirmed or early dialog
+	if sip.DialogState(state) != sip.DialogStateConfirmed && !s.isEarlyDialog() {
+		return fmt.Errorf("Dialog not confirmed or did not receive yet any response")
 	}
 
 	tx, err := s.TransactionRequest(ctx, bye)
@@ -459,6 +460,10 @@ func (s *DialogClientSession) WriteBye(ctx context.Context, bye *sip.Request) er
 	case <-ctx.Done():
 		return ctx.Err()
 	}
+}
+
+func (s *DialogClientSession) isEarlyDialog() bool {
+	return s.InviteResponse != nil && s.InviteResponse.IsProvisional()
 }
 
 // newAckRequestUAC creates ACK request for 2xx INVITE
